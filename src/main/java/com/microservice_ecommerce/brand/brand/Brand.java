a4674358d4abaf5832baeccf8d31e29acd57fb47
Salman <sallunasrullah@gmail.com>
2024-06-29 14:33:09 +0400

    lombok dependency added

--- src/main/java/com/microservice_ecommerce/brand/brand/Brand.java
+++ src/main/java/com/microservice_ecommerce/brand/brand/Brand.java
@@ -1,7 +1,12 @@
 package com.microservice_ecommerce.brand.brand;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -9,6 +14,10 @@ import java.util.Date;
 
 @Entity
 @Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Brand {
 
     @Id
@@ -16,6 +25,7 @@ public class Brand {
     private Long id;
 
     @Size(min = 2, max = 199)
+    @NotNull
     private String name;
 
     @Column(name = "created_at", updatable = false)
@@ -25,58 +35,5 @@ public class Brand {
     @Column(name = "updated_at")
     @UpdateTimestamp
     private Date updatedAt;
-
-    public Brand(Long id, String name, Date createdAt, Date updatedAt, Long productId) {
-        this.id = id;
-        this.name = name;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Brand() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public @Size(min = 2, max = 199) String getName() {
-        return name;
-    }
-
-    public void setName(@Size(min = 2, max = 199) String name) {
-        this.name = name;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Brand{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 
 }
